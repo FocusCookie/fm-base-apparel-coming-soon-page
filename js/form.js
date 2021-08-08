bffa90a3b710579077d3077974bd6a59f5e68ebe
FocusCookie <stephan_lueddemann@web.de>
2021-08-08 15:46:10 +0200

    added desktio view and js functions

--- js/form.js
+++ js/form.js
@@ -1,6 +1,7 @@
 const submit = document.getElementById("submit");
 const email = document.getElementById("email");
 const errorMsg = document.getElementById("errorMsg");
+const errorIcon = document.getElementById("errorIcon");
 
 const isValidMailRegex =
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -8,7 +9,9 @@ const isValidMailRegex =
 submit.addEventListener("click", () => {
   if (!email.value.match(isValidMailRegex)) {
     errorMsg.classList.remove("hidden");
+    errorIcon.classList.remove("hidden");
   } else {
     errorMsg.classList.add("hidden");
+    errorIcon.classList.add("hidden");
   }
 });
